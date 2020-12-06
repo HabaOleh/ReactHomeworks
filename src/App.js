9690c16566a66f0113e3bf4ee034beedd12aa42e
HabaOleh <gabikoleh25@gmail.com>
2020-12-06 15:12:21 +0200

    React Task 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import AllPost from "./components/AllPosts/AllPost";
 
 class App extends Component {
     render() {
         return (
             <div>
-                
+                <AllPost/>
             </div>
         );
     }
